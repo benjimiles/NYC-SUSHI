6040f4c92820cde9e1c4b2bafc31405143e74aa8
Fred Burke <fburke77@gmail.com>
2023-09-14 23:45:14 -0400

    temp data file and update to order

--- src/pages/Order.jsx
+++ src/pages/Order.jsx
@@ -3,17 +3,21 @@ import NavBar from '../components/NavBar';
 import Link from 'next/link';
 import 'tailwindcss/tailwind.css';
 import Footer from '../components/Footer';
-
+import jsonData from '../../FoodItem_data.json';
 const Order = () => {
   const [foodItems, setFoodItems] = useState([]);
+  
+useEffect(() => {
+  setFoodItems(jsonData);
+}, []);
   // Fetch Food Items from Django API
-  useEffect(() => {
-    fetch('http://localhost:8000/food-items/')
-      .then(response => response.json())
-      .then(data => setFoodItems(data.items))
+  // useEffect(() => {
+  //   fetch('http://localhost:8000/food-items/')
+  //     .then(response => response.json())
+  //     .then(data => setFoodItems(data.items))
 
-      .catch(error => console.error('Error fetching food items:', error));
-  }, []);
+  //     .catch(error => console.error('Error fetching food items:', error));
+  // }, []);
   return (
     <div className="flex flex-col min-h-screen">
       <NavBar />
@@ -30,12 +34,12 @@ const Order = () => {
           </p>
         </div>
         <div className="max-w-full mx-auto px-40 py-100 grid md:grid-cols-5 gap-10">
-          {foodItems.map(item => (
-            <div className="drop-shadow-2xl relative" key={item.id}>
+          {foodItems &&foodItems.map(item => (
+            <div className="drop-shadow-2xl relative" key={item.pk}>
               <div className="absolute border-solid border-2 border-white-500 bg-black/40 h-full rounded text-white text-center w-full">
-                <p className="font-bold text-2xl px-2 pt-10">{item.name}</p>
+                <p className="font-bold text-2xl px-2 pt-10">{item.fields.name}</p>
                 <p className="font-bold text-[15px] px-2">
-                  Price: ${Number(item.price).toFixed(2)}
+                  Price: ${Number(item.fields.price).toFixed(2)}
                 </p>
                 <button className="border hover:bg-white hover:text-black duration-300 text-white bg-black mx-2 bottom-4 rounded px-4 py-2 mt-5">
                   Place Order
@@ -43,8 +47,8 @@ const Order = () => {
               </div>
               <img
                 className="max-h-[160px] md:max-h-[200px] w-full object-cover rounded"
-                src={item.image_url}
-                alt={item.name}
+                src={item.fields.image_url}
+                alt={item.fields.name}
               ></img>
             </div>
           ))}
