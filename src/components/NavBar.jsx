164e56c6b7f0f1f83e733109eb515aa60170ac8b
Benji Miles <admin@codingwithbenji.com>
2023-09-16 18:04:48 +0200

    tried to add cart icon to navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,77 +1,128 @@
-'use client'
-import React, { useState, useEffect } from 'react'
-import Link from 'next/link'
+'use client';
+import React, { useState, useEffect } from 'react';
+import Link from 'next/link';
 import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
 import { useAuth } from '@/AuthContext';
 const NavBar = () => {
-    const [nav, setNav] = useState(0);
-    const [isLoggedIn, setIsLoggedIn] = useAuth();
-    const setState = () => {
-        setNav(!nav)
-    }
-    
-    const handleLogout = () => {
-        localStorage.removeItem('token');
-        setIsLoggedIn(false);
-    };
-    
-    useEffect(() => {
-    }, [isLoggedIn]);
-    
-    return (
-        <div className="w-full h-[70px] bg-black">
-            <div className="flex max-w-[1400px] h-full mx-auto px-4 items-center justify-between">
-                <div className="cursor-pointer flex font-bold p-2 items-center text-xl">
-                    <img src='https://cdn-icons-png.flaticon.com/128/2252/2252076.png' className='w-14 mr-4 h-14'></img>
-                    <h1 className="text-white font-Cinzel text-2xl"><Link href="/">NEW YORK CITY SUSHI</Link></h1>
-                </div>
-                <div className="hidden md:flex">
-                    <ul className="font-bold flex items-center text-xl text-white">
-                        <li className="cursor-pointer mx-4"><Link href="/About">About</Link></li>
-                        <li className="cursor-pointer mx-4"><Link href="/Order">Order</Link></li>
-                        <li className="cursor-pointer mx-4"><Link href="/Contact">Contact</Link></li>
-                        <li className="cursor-pointer mx-4"><Link href="/Checkout">Checkout</Link></li>
-                        {isLoggedIn ? (
-                            <button className="cursor-pointer mx-4" onClick={handleLogout}>
-                                <div class="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
-                                    <svg class="absolute w-12 h-12 text-gray-400 -left-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
-                                </div>
-                            </button>
-                        ) : (
-                            <button className="cursor-pointer mx-4">
-                                <Link href="/Login">Login</Link>
-                            </button>
-                        )} </ul>
-                </div>
+  const [nav, setNav] = useState(0);
+  const [isLoggedIn, setIsLoggedIn] = useAuth();
+  const setState = () => {
+    setNav(!nav);
+  };
 
-                <div onClick={setState} className="md:hidden block">
-                    {nav ? <AiOutlineClose size={30} className="cursor-pointer text-white" /> : <AiOutlineMenu size={30} className="cursor-pointer text-white" />}
-                </div>
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    setIsLoggedIn(false);
+  };
 
-            <div className={nav ? "duration-300 w-full bg-gradient-to-r bg-black absolute top-[70px] left-0 text-center justify-center flex" : "absolute left-[-100%]"}>
-            <ul className="font-bold items-center text-xl text-white my-10">
-                <li className="cursor-pointer my-10"><Link href="/About">About</Link></li>
-                <li className="cursor-pointer mb-10"><Link href="/Order">Order</Link></li>
-                <li className="cursor-pointer mb-10"><Link href="/Contact">Contact</Link></li>
-                <li className="cursor-pointer mb-10"><Link href="/Checkout">Checkout</Link></li>
-                {isLoggedIn ? (
-                    <button className="cursor-pointer mb-10" onClick={handleLogout}>
-                        <div class="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
-                            <svg class="absolute w-12 h-12 text-gray-400 -left-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
-                        </div>
-                    </button>
-                ) : (
-                    <button className="cursor-pointer mb-10">
-                        <Link href="/Login">Login</Link>
-                    </button>
-                )}
-            </ul>
+  useEffect(() => {}, [isLoggedIn]);
+
+  return (
+    <div className="w-full h-[70px] bg-black">
+      <div className="flex max-w-[1400px] h-full mx-auto px-4 items-center justify-between">
+        <div className="cursor-pointer flex font-bold p-2 items-center text-xl">
+          <img src="https://cdn-icons-png.flaticon.com/128/2252/2252076.png" className="w-14 mr-4 h-14"></img>
+          <h1 className="text-white font-Cinzel text-2xl">
+            <Link href="/">NEW YORK CITY SUSHI</Link>
+          </h1>
         </div>
-        
+        <div className="hidden md:flex">
+          <ul className="font-bold flex items-center text-xl text-white">
+            <li className="cursor-pointer mx-4">
+              <Link href="/About">About</Link>
+            </li>
+            <li className="cursor-pointer mx-4">
+              <Link href="/Order">Order</Link>
+            </li>
+            <li className="cursor-pointer mx-4">
+              <Link href="/Contact">Contact</Link>
+            </li>
+            <li className="cursor-pointer mx-4">
+              <Link href="/Checkout">Checkout</Link>
+            </li>
+            {isLoggedIn ? (
+              <button className="cursor-pointer mx-4" onClick={handleLogout}>
+                <div class="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+                  <svg
+                    class="absolute w-12 h-12 text-gray-400 -left-1"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+                      clip-rule="evenodd"
+                    ></path>
+                  </svg>
+                </div>
+              </button>
+            ) : (
+              <button className="cursor-pointer mx-4">
+                <Link href="/Login">Login</Link>
+              </button>
+            )}{' '}
+          </ul>
         </div>
 
+        <div onClick={setState} className="md:hidden block">
+          {nav ? (
+            <AiOutlineClose size={30} className="cursor-pointer text-white" />
+          ) : (
+            <AiOutlineMenu size={30} className="cursor-pointer text-white" />
+          )}
+        </div>
+
+        <div
+          className={
+            nav
+              ? 'duration-300 w-full bg-gradient-to-r bg-black absolute top-[70px] left-0 text-center justify-center flex'
+              : 'absolute left-[-100%]'
+          }
+        >
+          <ul className="font-bold items-center text-xl text-white my-10">
+            <li className="cursor-pointer my-10">
+              <Link href="/About">About</Link>
+            </li>
+            <li className="cursor-pointer mb-10">
+              <Link href="/Order">Order</Link>
+            </li>
+            <li className="cursor-pointer mb-10">
+              <Link href="/Contact">Contact</Link>
+            </li>
+            <li className="cursor-pointer mb-10">
+              <Link href="/Checkout">Checkout</Link>
+            </li>
+            <li className="cursor-pointer mb-10">
+              <Link href="/Checkout">Checkout</Link>
+            </li>
+            {isLoggedIn ? (
+              <button className="cursor-pointer mb-10" onClick={handleLogout}>
+                <div class="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+                  <svg
+                    class="absolute w-12 h-12 text-gray-400 -left-1"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+                      clip-rule="evenodd"
+                    ></path>
+                  </svg>
+                </div>
+              </button>
+            ) : (
+              <button className="cursor-pointer mb-10">
+                <Link href="/Login">Login</Link>
+              </button>
+            )}
+          </ul>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
