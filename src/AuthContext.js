34875658352f6e6eea0d24125a8db12e79906dca
Fred Burke <fburke77@gmail.com>
2023-09-19 00:50:10 -0400

    changes to auth

--- src/AuthContext.js
+++ src/AuthContext.js
@@ -1,56 +1,58 @@
 'use client';
 import React, { createContext, useContext, useState, useEffect } from 'react';
 
-// Initialize the context with the same type of data you plan to put in it.
 const AuthContext = createContext({
   isLoggedIn: false,
-  setIsLoggedIn: () => { },
+  setIsLoggedIn: () => {},
   userData: null,
-  setUserData: () => { },
+  setUserData: () => {},
+  loginUser: () => {},
 });
 
 export function useAuth() {
   return useContext(AuthContext);
+}
 
-}
-async function loginUser(token) {
-  localStorage.setItem('token', token);
-  setIsLoggedIn(true);
-}
 export function AuthProvider({ children }) {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [userData, setUserData] = useState(null);
   const [loading, setLoading] = useState(true);
-  // This is just for debugging, to ensure the provider is working as expected
+
+  const loginUser = (token) => {
+    localStorage.setItem('token', token);
+    setIsLoggedIn(true);
+  };
+
   useEffect(() => {
     const fetchData = async () => {
-      if (typeof window !== 'undefined') {
-        const token = localStorage.getItem('token');
-        if (token) {
-          setIsLoggedIn(true);
+      const token = typeof window !== 'undefined' ? localStorage.getItem('token') : null;
+      if (token) {
+        setIsLoggedIn(true);
 
-          const res = await fetch('http://localhost:8000/users/me/', {
-            headers: {
-              Authorization: `Bearer ${token}`, // Using the previously retrieved token
-            },
-          });
+        const res = await fetch('http://localhost:8000/users/me/', {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
 
-          if (res.ok) {
-            const data = await res.json();
-            setUserData(data);
-          } else {
-            console.error('Failed to fetch user data, status:', res.status);
-          }
+        if (res.ok) {
+          const data = await res.json();
+          setUserData(data);
         } else {
-          setIsLoggedIn(false);
+          console.error('Failed to fetch user data, status:', res.status);
         }
+      } else {
+        setIsLoggedIn(false);
       }
+      setLoading(false);  // Set loading to false only after all operations are done
     };
+
     fetchData();
-    setLoading(false);
-  }, [typeof window !== 'undefined' ? localStorage.getItem('token') : null]); // Added the token as a dependency
+  }, []);  // Removing the dependency on the token
 
-
-  return <AuthContext.Provider value={{isLoggedIn, setIsLoggedIn, userData, setUserData,loginUser}}>
-    {!loading && children}</AuthContext.Provider>;
+  return (
+    <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn, userData, setUserData, loginUser }}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
 }
