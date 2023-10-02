a48fbde4dc590dc82f4f5f86828a9584a5917cde
Sean Yuan <ysean54@yahoo.com>
2023-10-01 21:15:45 -0400

    Touch a bit on this more

--- src/pages/Profile-settings.jsx
+++ src/pages/Profile-settings.jsx
@@ -373,7 +373,7 @@ export default function Example() {
 
               <div className="grid max-w-7xl grid-cols-1 gap-x-8 gap-y-10 px-4 py-16 sm:px-6 md:grid-cols-3 lg:px-8">
                 <div>
-                  <h2 className="text-base font-semibold leading-7 text-white">Delete account</h2>
+                  <h2 className="text-base font-semibold leading-7 text-black">Delete account</h2>
                   <p className="mt-1 text-sm leading-6 text-gray-400">
                     No longer want to use our service? You can delete your account here. This action is not reversible.
                     All information related to this account will be deleted permanently.
